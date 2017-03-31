3ca728a6ca9fbcda0cec161e9cc64d11fffb3c73
Amir Raminfar <findamir@gmail.com>
2017-03-31 09:11:20 -0700

    Fixes missing python

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,15 +9,13 @@ fi
 mkdir -p /root/.ssh
 ssh-keygen -f /root/.ssh/id_rsa -P ""
 
-echo "blah"
-
 if [ -e /hostssh/authorized_keys ]; then
     cp /hostssh/authorized_keys /hostssh/authorized_keys.bak 
 fi
 
 cat /root/.ssh/id_rsa.pub >> /hostssh/authorized_keys
 
-# ansible all -i "localhost," -m raw -a "apt-get install -y python-minimal"
+ansible all -i "localhost," -m raw -a "apt-get install -y python-minimal"
 ansible-playbook -i "localhost," /ansible/bootstrap.yml
 
 if [ -e /hostssh/authorized_keys.bak ]; then
